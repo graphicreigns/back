0e37c727a0719957ebe00dfc4d894af8f01e75f7
mao <maoyimin2007@gmail.com>
2015-05-12 11:45:32 +0200

    afficher et ajouter

--- graphic.js
+++ graphic.js
@@ -11,21 +11,31 @@ var SITES=db.collection("sites");
 //var FORMULA= db.collection("formula"); 
 var SC = db.collection("statcommand");
 
+
 router.post('/getsite',function(req,res){
 	SITES.find({},function(err,docs){
 		res.jsonp(docs);
 	});
 });
 
+/*
+ * param:
+ * 
+ * "date" - le milliseconds de date -- GetTime()
+ * "nb"   -  nb commande
+ * "id_site" -  le id du site
+ * */
+
 router.post('/putstat_command',function(req,res){
 	
 	var newCommand = Object.create(null);
 	
-	var date=new Date();
-	var nb=100;
+	var date=new Date(parseInt(req.body.date));
+	var nb=req.body.nb;
 	
 	newCommand.date=date;
-	newCommand.id_site=ObjectId("5102e6c63a81b414e086564e");
+	//newCommand.id_site=ObjectId("5102e6c63a81b414e086564e");
+	newCommand.id_site=ObjectId(req.body.id_site);
 	newCommand.nbcommand=nb;
 	
 	SC.save(newCommand,function(err,result){
@@ -34,12 +44,71 @@ router.post('/putstat_command',function(req,res){
 	
 });
 
+
+/*
+ * param:
+ * 
+ * "debut" - le milliseconds de debut -- GetTime()
+ * "fin"   -  le milliseconds de fin  -- GetTime()
+ * */
 router.post('/getstat_command',function(req,res){
 	
-	SC.find({},function(err,result){
-		console.log(result);
-		res.jsonp("ok_get");
-	});
+	var time_debut=parseInt(req.body.debut);
+	var time_fin=parseInt(req.body.fin);
+	//var date_debut=new Date();
+	//var date_fin= new Date();
+	
+	//var query_debut=new Date(date_debut.getFullYear(),date_debut.getMonth(),date_debut.getDate()-3);
+	//var query_fin=new Date(date_fin.getFullYear(),date_fin.getMonth(),date_fin.getDate());
+	
+	//console.log(query_debut);
+	//console.log(query_fin);
+	
+	var days=(time_fin - time_debut)/(24*60*60*1000);
+	//var days=5;
+	var count=0;
+	
+	var array_envoyer=[];
+	
+	async.whilst(
+		function () { return count < days; },
+		function (callback) {
+			var query_debut=new Date(date_debut.getFullYear(),date_debut.getMonth(),date_debut.getDate()-count);
+			var query_fin=new Date(date_debut.getFullYear(),date_debut.getMonth(),date_debut.getDate()+1-count);
+
+			SC.find({date:{ $gte: query_debut, $lt: query_fin}},function(err,result){
+				//console.log(count,result);
+	
+				if(result.length>0){
+					var oneday ={};
+					oneday.date=query_debut;
+									
+					async.forEach(result,function(elem,cb_docs){
+						SITES.findOne({_id:ObjectId(elem.id_site)},function(err,site){
+							oneday[site.name]=elem.nbcommand;
+							cb_docs(null);
+						});
+
+
+					},function(err){
+						array_envoyer.push(oneday);
+						count++;
+						callback(null,null);						
+					});
+				}
+				else{
+					count++;
+					callback(null,null);
+				}
+			});
+
+		},
+		function (err) {
+			res.jsonp(array_envoyer);
+		}
+	);
+		
+
 	
 });
 
